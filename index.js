e9945fcab7fd3a857d455d8b2e299609079d4ae1
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2022-10-17 17:42:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById("mymodal");
-var btn = document.getElementById("btn_modal_window");
-var close = document.getElementById("close_modal_window");
+var modal = document.getElementById("mymodal"); //<div class="modal" id="mymodal"> (main div)
+var btn = document.getElementById("btn_modal_window"); //<div class="modal_content">
+var close = document.getElementById("close_modal_window"); //<span class="close_modal">&times;</span>
 
 btn.onclick = function () {
     modal.style.display = "block";
