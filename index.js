7a1ac41795fb7c934237dacfe98c8ad2e035d050
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2022-10-17 17:41:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("mymodal");
+var btn = document.getElementById("btn_modal_window");
+var close = document.getElementById("close_modal_window");
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}  
